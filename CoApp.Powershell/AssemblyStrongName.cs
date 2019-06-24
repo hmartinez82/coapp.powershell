8ba0fe4db0889abb2ce5cb92d770cd6f36a9b6e6
Ofek Shilon <ofekshilon@gmail.com>
2019-06-24 15:31:38 +0300

    CoApp projects upgraded and built Each build bumps the assembly version - currently leaving it at 2.4.514

--- CoApp.Powershell/AssemblyStrongName.cs
+++ CoApp.Powershell/AssemblyStrongName.cs
@@ -1,12 +1,12 @@
 ﻿//-----------------------------------------------------------------------
 // <copyright company="CoApp Project">
-//     Copyright (c) 2010-2013 Garrett Serack and CoApp Contributors. 
+//     Copyright (c) 2010-2013 Garrett Serack and CoApp Contributors.
 //     Contributors can be discovered using the 'git log' command.
 //     All rights reserved.
 // </copyright>
 // <license>
 //     The software is licensed under the Apache 2.0 License (the "License")
-//     You may not use the software except in compliance with the License. 
+//     You may not use the software except in compliance with the License.
 // </license>
 //-----------------------------------------------------------------------
 
@@ -16,12 +16,12 @@ using System.Reflection;
 [assembly: AssemblyCompany("Outercurve Foundation")]
 [assembly: AssemblyCopyright("Copyright (c) Garrett Serack, Contributors 2010-2013")]
 
-// We no longer need to delay sign in order to strong name and sign the code before 
-// we publish it, so now we will have just one set of  Version  lines, and no strong 
+// We no longer need to delay sign in order to strong name and sign the code before
+// we publish it, so now we will have just one set of  Version  lines, and no strong
 // naming until publishing.
 
-[assembly: AssemblyVersion("2.4.501.0")]
-[assembly: AssemblyFileVersion("2.4.501.0")]
+[assembly: AssemblyVersion("2.4.514.0")]
+[assembly: AssemblyFileVersion("2.4.514.0")]
 [assembly: AssemblyProduct("CoApp Developer Powershell CmdLets")]
 [assembly: AssemblyTrademark("CoApp is a non-registered trademark of Garrett Serack")]
 
--- build-release.cmd
+++ build-release.cmd
@@ -27,8 +27,8 @@ echo Merging Monolithic build for .NET 4.0 [ClrPlus.dll]
 %TOOLSDIR%\il-repack.exe /log:merge.log /t:library /xmldocs /targetplatform:v4 /lib:"C:\Program Files (x86)\Reference Assemblies\Microsoft\WindowsPowerShell\3.0" /out:ClrPlus.dll ClrPlus.CommandLine.dll ClrPlus.Console.dll ClrPlus.Core.dll  ClrPlus.Crypto.dll ClrPlus.Networking.dll ClrPlus.Platform.dll ClrPlus.Powershell.Core.dll ClrPlus.Powershell.Provider.dll ClrPlus.Remoting.dll ClrPlus.Scripting.dll ClrPlus.Scripting.MsBuild.dll ClrPlus.Windows.Api.dll ClrPlus.Windows.Debugging.dll ClrPlus.Windows.PeBinary.dll Microsoft.Cci.MetadataHelper.dll Microsoft.Cci.MetadataModel.dll Microsoft.Cci.MutableMetadataModel.dll Microsoft.Cci.PeReader.dll Microsoft.Cci.PeWriter.dll Microsoft.Cci.SourceModel.dll Microsoft.WindowsAzure.Storage.dll System.Spatial.dll  ClrPlus.Powershell.Azure.dll Microsoft.Threading.Tasks.dll Microsoft.Threading.Tasks.Extensions.dll Microsoft.Threading.Tasks.Extensions.Desktop.dll System.Runtime.dll System.Threading.Tasks.dll  || goto failed
 
 REM for %%v in (clrplus*.dll) do "C:\Program Files (x86)\Windows Kits\8.0\bin\x86\signtool.exe" sign /a /t http://timestamp.verisign.com/scripts/timstamp.dll %%v || goto fin
-popd 
-        
+popd
+
 echo Merging Monolithic builds for .NET 4.5
 pushd output\v45\AnyCPU\Release\bin
 
@@ -54,13 +54,13 @@ nuget pack  clrplus\Remoting\Remoting.nuspec
 nuget pack  clrplus\Scripting\Scripting.nuspec
 nuget pack  clrplus\Scripting.MsBuild\Scripting.MsBuild.nuspec
 nuget pack  clrplus\Windows.Debugging\Windows.Debugging.nuspec
-nuget pack  clrplus\Windows.PeBinary\Windows.PeBinary.nuspec        
-nuget pack  clrplus\Powershell.Core\Powershell.Core.nuspec        
-rem nuget pack  clrplus\Powershell.Rest\Powershell.Rest.nuspec        
-nuget pack  clrplus\Powershell.Provider\Powershell.Provider.nuspec        
-nuget pack  clrplus\Powershell.Azure\Powershell.Azure.nuspec        
-nuget pack  clrplus\ClrPlus.Powershell.nuspec   
-nuget pack  clrplus\ClrPlus.nuspec  
+nuget pack  clrplus\Windows.PeBinary\Windows.PeBinary.nuspec
+nuget pack  clrplus\Powershell.Core\Powershell.Core.nuspec
+rem nuget pack  clrplus\Powershell.Rest\Powershell.Rest.nuspec
+nuget pack  clrplus\Powershell.Provider\Powershell.Provider.nuspec
+nuget pack  clrplus\Powershell.Azure\Powershell.Azure.nuspec
+nuget pack  clrplus\ClrPlus.Powershell.nuspec
+nuget pack  clrplus\ClrPlus.nuspec
 
 goto FIN
 
